ac51c64f1830318dfb8868951e34dd2b18a62c74
Zachary <hello@zach.ie>
2015-10-14 22:43:20 +0100

    Update client to show received response

--- client.py
+++ client.py
@@ -3,20 +3,16 @@ import sys
 from time import sleep
 from random import randrange
 
-# create socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # set GET message
-data = "helo\n"
+data = "HELO asd\n"
 
-# connect to server and send messag
 for i in range(1000):
-    print "sending" + str(i)
+    print "Sending #" + str(i)
+    # connect to socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("localhost", 61234)) 
+    # send data
     s.sendall(data)
-
-# print sent and received data
-received = s.recv(1024)
-print "Sent:     {}".format(data)
-print "Received: {}".format(received)
+    # print received response
+    received = s.recv(1024)
+    print "Received: {}".format(received)
