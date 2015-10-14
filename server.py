040cb39dbd38d9c61c2ef310be11afe0abf8d441
Zachary <hello@zach.ie>
2015-10-14 22:43:08 +0100

    Add pattern matching to received data

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 import sys
 import socket
+import re
 from Queue import Queue
-from time import sleep
 from threading import Thread
 
 # from http://code.activestate.com/recipes/577187-python-thread-pool/
 class Worker(Thread):
     """Thread executing tasks from a given tasks queue"""
-    def __init__(self, clients, thread_id):
+    def __init__(self, clients):
         Thread.__init__(self)
+        # store clients queue pointer
         self.clients = clients
-        self.thread_id = thread_id
+        # set as daemon so it dies when main thread exits
         self.daemon = True
+        # start the thread on init
         self.start()
     
     def run(self):
@@ -20,20 +22,19 @@ class Worker(Thread):
             (conn, data) = self.clients.get()
             # check if connection or kill request
             if conn:
-                process_req(self.thread_id, conn, data, self.clients);
+                ip, port = conn.getpeername()
+                match = re.match("HELO (.*)\n", data)
+                # check if kill command or helo message
+                if data == "KILL_SERVICE\n":
+                    self.interrupt_main()
+                elif match is not None:
+                    conn.sendall("HELO " + match.groups()[0] + "\nIP:" + ip + "\nPort:" + str(port) + "\nStudentID:\n")
+                # close connection
+                conn.close()
             else:
                 break;
             # set task as done in queue
             self.clients.task_done()
-
-# function to process a client request
-def process_req(thread_id, conn, data, clients):
-    sleep(0.5)
-    ip, port = conn.getpeername()
-    # return data to client
-    conn.sendall(data)
-    # close connection
-    conn.close()
 
 if len(sys.argv) != 2 or not sys.argv[1].isdigit():
     sys.exit("Port number required")
@@ -61,12 +62,14 @@ s.listen(1)
 
 # create initial workers
 for i in range(min_threads): 
-    Worker(clients, i)
+    Worker(clients)
 
-# loop through all data received
+# continuous loop to keep accepting requests
 while 1:
     # accept a connection request
     conn, addr = s.accept()
+
+    # cache queue size and get threshold
     qsize = clients.qsize()
     queue_margin = (num_threads * queue_threshold / 100)
 
@@ -76,20 +79,19 @@ while 1:
         for i in range(queue_margin): 
             if num_threads == max_threads:
                 break
+            Worker(clients)
             num_threads += 1
-            Worker(clients, num_threads)
         # print to console that we're bumping the size
         if num_threads != max_threads:
             print "[MAX] bumping queue size! by " + str(queue_margin) + " to " + str(num_threads)
-
     # else check if queue size is between 0 and margin
     elif qsize <= queue_margin:
         # remove queue_margin amount of workers
         for i in range(queue_margin): 
             if num_threads == min_threads:
                 break
-            num_threads -= 1
             clients.put((None, None))
+            num_threads -= 1
         # print to console that we're decreasing the size
         if num_threads != min_threads:
             print "[MIN] decreasing queue size! by " + str(queue_margin) + " to " + str(num_threads)
